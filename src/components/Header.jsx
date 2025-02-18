ce87252d565cd7ac639c6be8a5f201b05272711c
alessandro-sartini <ale.sartini.98@gmail.com>
2025-02-18 16:56:35 +0100

    add:main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,32 @@
 import logoHeader from "../assets/img/dc-logo.png"
 
-// const Header = () => {
-//     return (
-       
 
-
-   
-//         <header>
-//             <div className="logo">
-//                 <img src={logoHeader} alt="" />
-//         </div>
-//         <nav>
-//             <ul>
-//                 <li><a href="#">Characters</a></li>
-//                 <li><a href="#">Comics</a></li>
-//                 <li><a href="#">Movies</a></li>
-//                 <li><a href="#">TV</a></li>
-//                 <li><a href="#">Games</a></li>
-//                 <li><a href="#">Collectibles</a></li>
-//                 <li><a href="#">Videos</a></li>
-//                 <li><a href="#">Fans</a></li>
-//                 <li><a href="#">News</a></li>
-//                 <li><a href="#">Shop</a></li>
-//             </ul>
-//         </nav>
-//     </header>
-    
-//   )
-
-// }
 
 const Header = () => {
 
     return (
             
         <header className="bg-white shadow-sm py-3">
-        <div className="container d-flex justify-content-between align-items-center">
-            <div className="logo">
-            <img src={logoHeader} alt="" />
+            <div className="container d-flex justify-content-between align-items-center">
+                <div className="logo">
+                <img src={logoHeader} alt="" />
+                </div>
+                <nav>
+                    <ul className="nav">
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">Characters</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">Comics</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">Movies</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">TV</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">Games</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">Collectibles</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">Videos</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">Fans</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">News</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="#">Shop</a></li>
+                    </ul>
+                </nav>
             </div>
-            <nav>
-                <ul className="nav">
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">Characters</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">Comics</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">Movies</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">TV</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">Games</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">Collectibles</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">Videos</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">Fans</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">News</a></li>
-                    <li className="nav-item"><a className="nav-link fw-bold" href="#">Shop</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+        </header>
     
 
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,45 @@
+import digital from "../assets/img/buy-comics-digital-comics.png"
+import merch from "../assets/img/buy-comics-merchandise.png"
+import shop from "../assets/img/buy-comics-shop-locator.png"
+import subScription from "../assets/img/buy-comics-subscriptions.png"
+import power from "../assets/img/buy-dc-power-visa.svg"
+
+const Main = () => {
+
+    return (
+            
+        <main className="">
+
+            <section className="bg-dark ">
+
+                <h2 className="p-5 fw-bold text-white ">  Content goes here... </h2>
+
+
+            </section>
+            <section class="bg-primary text-white py-3">
+                <div class="container text-center">
+                    <ul class="list-inline m-3">
+                        <img src={digital} alt="" height={"50px"} /><li class="list-inline-item px-3"><a class="text-white text-decoration-none" href="#">Digital Comics</a></li>
+                        <img src={merch} alt="" height={"50px"}/><li class="list-inline-item px-3"><a class="text-white text-decoration-none" href="#">DC Merchandise</a></li>
+                        <img src={shop} alt="" height={"50px"}/><li class="list-inline-item px-3"><a class="text-white text-decoration-none" href="#">Subscription</a></li>
+                        <img src={subScription} alt="" height={"50px"}/><li class="list-inline-item px-3"><a class="text-white text-decoration-none" href="#">Comic Shop Locator</a></li>
+                        <img src={power} alt="" height={"30px"}/><li class="list-inline-item px-3"><a class="text-white text-decoration-none" href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </section>
+        </main>
+    
+
+
+
+
+        )
+
+
+
+
+
+}
+
+export default Main
+
